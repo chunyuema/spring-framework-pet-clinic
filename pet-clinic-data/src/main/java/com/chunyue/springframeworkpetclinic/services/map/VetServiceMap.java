559041cb8c7a27cf9a064e6fd4679831eeeaec41
Chunyue Ma <cma01@wesleyan.edu>
2021-08-17 22:01:15 -0400

    persist the specialities when saving vets #14

--- pet-clinic-data/src/main/java/com/chunyue/springframeworkpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/chunyue/springframeworkpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.chunyue.springframeworkpetclinic.services.map;
 
+import com.chunyue.springframeworkpetclinic.model.Specialty;
 import com.chunyue.springframeworkpetclinic.model.Vet;
+import com.chunyue.springframeworkpetclinic.services.SpecialtyService;
 import com.chunyue.springframeworkpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialties().size()> 0){
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null){
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
