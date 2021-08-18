ee5b5f335ca4ea8132da5e725b5f23c0587b70e3
Chunyue Ma <cma01@wesleyan.edu>
2021-08-17 21:22:45 -0400

    add specialty map services

---
+++ pet-clinic-data/src/main/java/com/chunyue/springframeworkpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.chunyue.springframeworkpetclinic.services;
+
+import com.chunyue.springframeworkpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+
+}
